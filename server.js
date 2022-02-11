295a194034aea3f2c5f3cb977daacb188d384c84
Justin Garner <j.garner828@gmail.com>
2022-02-11 07:22:30 -0500

    fixed server.js

--- server.js
+++ server.js
@@ -40,11 +40,3 @@ sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
-
-// Ask team about doing this instead
-// app.engine('handlebars', handlebars({
-//   defaultLayout: 'main',
-//   layoutsDir: path.join(__dirname, '/views/layouts')
-// }));
-// app.set('views', path.join(__dirname, '/views'));
-// app.set('view engine', 'handlebars');
