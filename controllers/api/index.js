c13131a22637fd83b2f3047caba8f88c078a47cc
John Quintanilla <jq150010@gmail.com>
2022-02-14 14:03:33 -0500

    john's commits 2/14

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-
+const competitionRoutes = require('./competitionRoutes')
 
 router.use('/users', userRoutes);
-
+router.use('/competitions', competitionRoutes)
 
 
 module.exports = router;
