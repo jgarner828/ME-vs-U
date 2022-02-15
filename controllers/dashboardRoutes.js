9eac94160a4e9d584a3f23fc9222a8003ef8fe2b
John Quintanilla <jq150010@gmail.com>
2022-02-15 15:23:31 -0500

    2/15, third commit

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const { Scoreboard, Competition, User } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
@@ -10,5 +11,73 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/hydration", async (req, res) => {
+  try {
+    const scoreboardData = await Scoreboard.findByPk(req.session.user_id, {
+      include: [
+        {
+          model: Competition,
+          attributes: ["title"],
+        },
+      ],
+    });
+
+    const hydration = scoreboardData.get({ plain: true });
+
+    res.render("hydration", {
+      ...hydration,
+      logged_in: req.session.logged_in,
+    });
+    console.log(scoreboardData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/daily-steps", async (req, res) => {
+  try {
+    const scoreboardData = await Scoreboard.findByPk(req.session.user_id, {
+      include: [
+        {
+          model: Competition,
+          attributes: ["title"],
+        },
+      ],
+    });
+
+    const dailysteps = scoreboardData.get({ plain: true });
+
+    res.render("dailysteps", {
+      ...dailysteps,
+      logged_in: req.session.logged_in,
+    });
+    console.log(scoreboardData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/weekly-miles", async (req, res) => {
+  try {
+    const scoreboardData = await Scoreboard.findByPk(req.session.user_id, {
+      include: [
+        {
+          model: Competition,
+          attributes: ["title"],
+        },
+      ],
+    });
+
+    const weeklymiles = scoreboardData.get({ plain: true });
+
+    res.render("weeklymile", {
+      ...weeklymiles,
+      logged_in: req.session.logged_in,
+    });
+    console.log(scoreboardData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
