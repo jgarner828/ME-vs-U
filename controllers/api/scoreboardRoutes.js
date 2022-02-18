e377a138782aa29e8e390d2b5dbf911139e848c1
Justin Garner <j.garner828@gmail.com>
2022-02-18 13:51:06 -0500

    adding update score functionality

--- controllers/api/scoreboardRoutes.js
+++ controllers/api/scoreboardRoutes.js
@@ -1,4 +1,5 @@
 const { Scoreboard } = require("../../models");
+const { sequelize } = require("../../models/User");
 const router = require("express").Router();
 const withAuth = require("../../utils/auth");
 
@@ -24,5 +25,43 @@ router.put('/:id', withAuth, async (req, res) => {
 });
 
 
+router.post('/updatescore', withAuth, async (req, res) => {
+
+try {
+// user id
+const user_id = req.session.user_id;
+
+// competition id
+let competitionId = req.headers.referer;
+let competitionToSplit = competitionId.split('competitions/');
+let splitForId = competitionToSplit[1].split('?');
+competitionId = splitForId[0];
+
+// user score update
+const { updateValue } = req.body;
+
+const updateScore = await Scoreboard.update(
+  { score: sequelize.literal(`score + ${updateValue}`) },
+   { where: {
+          user: `${user_id}`,
+          competition_id: `${competitionId}`,
+          }
+  })
+
+  if(!updateScore) {
+    res.status(500).json('Error updating score')
+  } else {
+    res.redirect('/');
+  }
+
+
+  
+} catch (error) {
+  res.status(500).json(error)
+}
+
+
+
+});
   
   module.exports = router
