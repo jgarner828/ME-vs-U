949a900e82d74d3f85ce40e21aee39706398f7cc
Narcisah <91091964+mel-ificent@users.noreply.github.com>
2022-02-18 00:31:26 -0500

    added helper methods for days remaining, and determining if a competition is active. Removed isPublic from competition model

--- utils/helpers.js
+++ utils/helpers.js
@@ -8,4 +8,24 @@ module.exports = {
             return false;
         }
     },
+
+    timeRemaining: (end_date, start_date) => {
+     let timeRemaining = (end_date - start_date)/86400000;
+  
+        if(isNaN(timeRemaining)){
+            return 0;
+        }
+        else{ return timeRemaining
+        };
+    },
+    isActive:(end_date, start_date) => {
+        let timeRemaining = (end_date - start_date)/86400000;
+     
+           if(isNaN(timeRemaining)){
+               return false;
+           }
+           else{ return true;
+           };
+       },
+
   };
