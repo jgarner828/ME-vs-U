817db7e7e2feb15132c7d82932f643c0437f1f61
Narcisah <91091964+mel-ificent@users.noreply.github.com>
2022-02-19 12:36:20 -0300

    updated submit button

--- public/js/invitePeople.js
+++ public/js/invitePeople.js
@@ -1,18 +1,45 @@
-const getUsers =  function(event) {
-  event.preventDefault();
+// const url= window.location.href;
+// const users=[];
+// console.log(comp_id);
 
-    
-// fetch('/api/users/all')
-//   .then(response => response.json())
-//   .then( (data) => {
-//       console.log(data);
-//   });
+const getUsers =  function(event) {
+    // event.preventDefault();
+  
+    // // Collect values from the login form
+    // const user_id = 1;
+    // const score = 0;
+    // const competition_id = 20;
+
+
+    //   // Send a POST request to the API endpoint
+    //   const response = await fetch("/api/scoreboard/add", {
+    //     method: "POST",
+    //     body: JSON.stringify({
+    //       user_id,
+    //       score,
+    //       competition_id,
+    //     }),
+    //     headers: { "Content-Type": "application/json" },
+    //   });
+  
+    //   if (response.ok) {
+  
+    //     document.location.replace('/dashboard');
+  
+    //   } else {
+    //     alert(response.statusText);
+    //     console.log("unsuccessful");
+    //   }
+
+      document.location.replace('/dashboard');
+    };
+ 
+
 
   document
-  .querySelector("#createButton")
-  .addEventListener("click", getUsers);
-}
+  .querySelector("#add-users")
+  .addEventListener("submit", getUsers);
 
 
-console.log("helloo")
+
 
